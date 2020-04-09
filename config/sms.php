91e2c84870005ff6d20d4aa7d0c4eb2f85ba0a11
Shadow <admin@hongyuvip.com>
2020-04-09 13:02:13 +0800

    修改短信默认配置文件

--- config/sms.php
+++ config/sms.php
@@ -74,21 +74,19 @@ return [
     ],
 
     'templates' => [
+        'aliyun' => [
+            'verify_code' => env('SMS_ALIYUN_VERIFICATION_CODE'),
+        ],
         'yunpian' => [
             'verify_code' => env('SMS_YUNPIAN_VERIFICATION_CODE'),
-            'user_account_pay' => '',
-            'user_order_deliver_goods' => '',
-            'seller_new_order' => '',
-            'seller_order_account_pay' => '',
         ],
-        'duanxinbao' =>
-            [
-                'verify_code' => '您的验证码是%s。有效期为15分钟，请尽快验证！',
-                'user_account_pay' => '已经收到您的订单，订单编号：%s，我们将尽快给您发货。',
-                'user_order_deliver_goods' => '您的订单已发货，订单编号：%s，收货人：%s，联系电话：%s，收货地址：%s，请注意查收！',
-                'seller_new_order' => '您有一条新订单，订单编号：%s，请注意查看。',
-                'seller_order_account_pay' => '客户已付款，订单编号：%s，收货人：%s，联系电话：%s，订单金额：%s。',
-            ],
+        'duanxinbao' => [
+            'verify_code' => '您的验证码是%s。有效期为15分钟，请尽快验证！',
+        ],
+        'chuanglan' => [
+            'verify_code' => '您的验证码是{$var}。有效期为15分钟，请尽快验证！',
+        ],
+
         // Other...
     ],
 
